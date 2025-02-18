03ddb5ba9108b6fe97c14ede9a2989f6ae8ddd47
Rodkool <rodkool03@gmail.com>
2025-02-18 17:58:09 +0100

    Timer termine

--- src/app/inclusif-cards/navigation-card/navigation-card.component.ts
+++ src/app/inclusif-cards/navigation-card/navigation-card.component.ts
@@ -69,4 +69,10 @@ export class NavigationCardComponent {
   hideDialog() {
     this.displayDialog = false;
   }
+
+  formatTime(seconds: number): string {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+  }
 }
