d85b726f9487399249a244f1d421e4cd8e5cd53e
holliape <154310956+holliape@users.noreply.github.com>
2025-02-25 18:07:39 +0100

    solutions

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,7 +9,7 @@ import { RulesSolutionsComponent } from './views/rules-solutions/rules-solutions
 import { UserManagerComponent } from './views/user-manager/user-manager.component';
 import { InclusifCardsComponent } from './views/inclusif-cards/inclusif-cards.component';
 import { NavigationCardComponent } from './views/inclusif-cards/navigation-card/navigation-card.component';
-
+import { SolutionsComponent } from './views/solutions/solutions.component';
 
 export const routes: Routes = [
     {path: 'accueil', component: HomepageComponent},
@@ -30,6 +30,7 @@ export const routes: Routes = [
             },
             { path: '**', redirectTo: 'cartes_inclusif' }  // Rediriger les routes invalides
           ]},
+    {path:'solutions',component:SolutionsComponent},
     { path: '**',
         redirectTo: 'accueil',
         pathMatch: 'full'
