3e8b5d1e2dcaa14f7eac29485245c371b7471ae4
holliape <154310956+holliape@users.noreply.github.com>
2025-02-27 09:52:23 +0100

    on avance, c'est une évidence

--- src/app/views/rules-solutions/rules-solutions.component.ts
+++ src/app/views/rules-solutions/rules-solutions.component.ts
@@ -11,6 +11,10 @@ import { ActionBarComponent } from '../../shared/action-bar/action-bar.component
 export class RulesSolutionsComponent {
   constructor(private router:Router) {}
     continuer(){
-      this.router.navigate(['./solution'])
+      this.router.navigate(['./solution'], {
+        queryParams: { numero: 0},
+        queryParamsHandling: 'merge', // Merge avec les paramètres existants
+        skipLocationChange: false // Mettre à jour l'URL dans la barre d'adresse
+      })
     }
 }
