5bc2c65047829e52de19b63e482b0d95354fb101
Rodkool <rodkool03@gmail.com>
2025-03-25 11:14:32 +0100

    excel fini?????!!!!

--- src/app/views/excel/excel.component.ts
+++ src/app/views/excel/excel.component.ts
@@ -15,14 +15,11 @@ import { navigation_data_solutions } from "../../app.component";
 })
 
 export class ExcelComponent {
-  
-  //Ceci est un exemple, à remplacer avec le vrai excel qui sera contenu dans le service j'imagine
-  users : any;
+  users: any;
+  maxTaskIndexes: number[] = [];
 
+  constructor(private router: Router, private service: UserService) {}
 
-
-  constructor(private router:Router, private service:UserService) {}
-  
   ngOnInit(): void {
     this.users = this.service.getUsers();
 
@@ -30,36 +27,43 @@ export class ExcelComponent {
     const answersData = navigation_data_solutions.data;
 
     this.users.forEach((user: any) => {
-        if (user.getTask) {
-            const tasks = user.getTask();
-            tasks.forEach((task: any) => {
-                const taskData = answersData.find((data: any) => data.id === task.getId());
-                if (taskData) {
-                    // Replace the task ID with the category name and title
-                    task.id = `${taskData.categorie} - ${taskData.titre}`;
-                }
-            });
-        }
+      if (user.getTask) {
+        const tasks = user.getTask();
+        tasks.forEach((task: any) => {
+          const taskData = answersData.find((data: any) => data.id === task.getId());
+          if (taskData) {
+            // Replace the task ID with the category name and title
+            task.id = `${taskData.categorie} - ${taskData.titre}`;
+          }
+        });
+      }
     });
+
+    // Calculate the maximum number of tasks
+    const maxTasks = Math.max(...this.users.map((user: any) => user.getTask().length));
+    this.maxTaskIndexes = Array.from({ length: maxTasks }, (_, i) => i);
   }
 
-  continuer(){
-    this.router.navigate(['./regles_conclusion'])
+  emptyColumns(taskCount: number): number[] {
+    const maxTasks = this.maxTaskIndexes.length;
+    return Array.from({ length: maxTasks - taskCount });
   }
 
-  exportexcel(): void
-  {
+  continuer() {
+    this.router.navigate(['./regles_conclusion']);
+  }
+
+  exportexcel(): void {
     /* pass here the table id */
     let element = document.getElementById('excel-table');
-    const ws: XLSX.WorkSheet =XLSX.utils.table_to_sheet(element);
- 
+    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
+
     /* generate workbook and add the worksheet */
     const wb: XLSX.WorkBook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
- 
-    /* save to file */  
-    XLSX.writeFile(wb, "Resultats_JEU.xlsx");
- 
+
+    /* save to file */
+    XLSX.writeFile(wb, 'Resultats_JEU.xlsx');
   }
 }
 
